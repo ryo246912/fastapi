8db7134631abeb35b8e203bf75954fe5f2fa93de
ryo246912 <r-k.sky_25cloudf@hotmail.co.jp>
2022-07-23 17:52:26 +0900

    [add]: 型定義・Union
    ・クエリパラメータ有無による条件分け
    ・Unionなど

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from enum import Enum
+from typing import Union
 
 app = FastAPI()
 
@@ -19,9 +20,20 @@ def read_root():
     return {"Hello": "World"}
 
 
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: str = None):
+#     return {"item_id": item_id, "q": q}
+
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+async def read_item(item_id: str, q: Union[str, None] = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
 
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
